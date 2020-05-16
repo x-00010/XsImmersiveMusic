071e230654717ddd0eafefbb1511d08eb39171a5
52288074 <x00010@protonmail.com>
2020-05-16 12:15:32 +0100

    Added mod.cpp and image

---
+++ addons/XIM_Core/mod.cpp
@@ -0,0 +1,12 @@
+name = "XIM - X's Immersive Music";
+picture = "\XIM_Core\XIMCore.paa";
+actionName = "Steam";
+action = "https://steamcommunity.com/sharedfiles/filedetails/?id=2097412544";
+description = "A mod which aims to add dynamic music to Arma 3!";
+logo = "\XIM_Core\XIMCore.paa";
+logoOver = "\XIM_Core\XIMCore.paa;
+tooltip = "XIM - X's Immersive Music";
+tooltipOwned = "";
+overview = "A mod which aims to add dynamic music to Arma 3!";
+author = "x00010";
+overviewPicture = "\XIM_Core\XIMCore.paa;
