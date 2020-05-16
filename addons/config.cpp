9202f0fdc902285a407757fd38aa96cc061eaee8
x-00010 <52288074+x-00010@users.noreply.github.com>
2020-05-16 10:28:41 +0100

    Add files via upload

---
+++ addons/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches
+{
+	class XIMCore
+	{
+		name = "X's Immersive Music";
+		author = "x00010";
+		requiredVersion = 1.98;
+		requiredAddons[] = { 
+			"cba_keybinding" 
+		};
+	};
+};
