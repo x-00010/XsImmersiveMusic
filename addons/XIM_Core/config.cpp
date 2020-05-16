16a2622b635dfcbb881e8ae1a74d2d913851b70f
52288074 <x00010@protonmail.com>
2020-05-16 19:27:35 +0100

    Basic script init test

--- addons/XIM_Core/config.cpp
+++ addons/XIM_Core/config.cpp
@@ -5,8 +5,16 @@ class CfgPatches
 		name = "X's Immersive Music";
 		author = "x00010";
 		requiredVersion = 1.98;
-		requiredAddons[] = { 
-			"cba_keybinding" 
+		requiredAddons[] = {  
+			"cba_settings"
 		};
 	};
+};
+
+class Extended_PreInit_EventHandlers 
+{
+    class XIMCoreStart // preinit event handler
+	{
+        init = "call compile preprocessFileLineNumbers '\XIM_Core\scripts\autostart\XEH_PreInit.sqf'";
+    };
 };
